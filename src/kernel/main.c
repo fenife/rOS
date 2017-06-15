7078a758182050b1ad3699b10ae2cbeebabd3747
feng <896151545@qq.com>
2017-06-15 19:35:43 +0800

    用实现的put_char()函数打印字符

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,9 +1,24 @@
 /* main.c
- * 第一个内核C语言代码，演示elf文件解析以及加载内核 
+ *   用实现的put_char()函数打印字符
  */
-int main(void)
+
+#include <sys/print.h>
+
+void main(void)
 {
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+    
+    put_char('1');
+    put_char('2');
+    put_char('\b');
+    put_char('3');
+    
     while (1)
         ;
-    return 0;
 }
