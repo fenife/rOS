c2bb25421e50a4a331ce84ff8c70e1bd13cbcf0d
feng <896151545@qq.com>
2017-06-17 12:42:35 +0800

    测试中断处理

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,20 +3,14 @@
  */
 
 #include <sys/print.h>
+#include <sys/init.h>
 
 void main(void)
 {
     put_str("I am kernel\n");
-    put_int(0);
-    put_char('\n');
-    put_int(9);
-    put_char('\n');
-    put_int(0x00021a3f);
-    put_char('\n');
-    put_int(0x12345678);
-    put_char('\n');
-    put_int(0x0);
-    put_char('\n');
+    init_all();     /* 初始化所有模块 */
+
+    asm volatile ("sti");   /* 测试中断处理，临时开中断 */
 
     while (1)
         ;
