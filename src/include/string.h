440fbb662c26ef89dab02b08dc8981c53b80447f
feng <896151545@qq.com>
2017-06-18 17:13:16 +0800

    增加头文件包含

--- src/include/string.h
+++ src/include/string.h
@@ -6,6 +6,8 @@
 #ifndef __LIB_STRING_H
 #define __LIB_STRING_H
 
+#include <stddef.h>
+
 void * memchr(const void *s, int c, size_t n);
 int    memcmp(const void *s1, const void *s2, size_t n);
 void * memcpy(void *s1, const void *s2, size_t n);
