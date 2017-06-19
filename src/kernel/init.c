0716388de6975a50baad4497d322e0a9deeef3e2
feng <896151545@qq.com>
2017-06-19 11:39:17 +0800

    内存管理系统之一：内存池规划

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -6,6 +6,7 @@
 #include <sys/interrupt.h>
 #include <sys/print.h>
 #include <dev/timer.h>
+#include <sys/memory.h>
 
 /* 负责初始化所有模块 */
 void init_all(void)
@@ -13,5 +14,6 @@ void init_all(void)
     put_str("init_all start ... \n");
     idt_init();     /* 初始化中断 */
     timer_init();   /* 初始化定时器/计数器，设置时钟中断频率 */
+    mem_init();     /* 初始化内存管理系统 */
     put_str("init_all done.\n");
 }
