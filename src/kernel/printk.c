d8ec38ef95f0e6a8615894c4f231c1446dabe4b5
feng <896151545@qq.com>
2017-06-22 08:42:54 +0800

    修复printk中buf的同步竞争问题

--- src/kernel/printk.c
+++ src/kernel/printk.c
@@ -5,18 +5,19 @@
 #include <stdarg.h>
 #include <console.h>
 #include <printk.h>
-
-static char buf[1024];
+#include <print.h>
 
 int printk(const char *fmt, ...)
 {
 	va_list args;
 	int i;
-
+    char buf[1024] = { 0 };
+    
 	va_start(args, fmt);
 	i = vsprintf(buf,fmt,args);
 	va_end(args);
 	console_put_str(buf);
+
 	return i;
 }
 
