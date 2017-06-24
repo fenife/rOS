7107e17e3b0d40a753f4eb354a5fb51e1083c940
feng <896151545@qq.com>
2017-06-24 15:32:31 +0800

    实现printf

---
+++ src/include/stdio.h
@@ -0,0 +1,11 @@
+/* stdio.h
+ */
+
+#ifndef __LIB_STDIO_H
+#define __LIB_STDIO_H
+
+#include <stdint.h>
+
+uint32_t printf(const char *fmt, ...);
+
+#endif  /* __LIB_STDIO_H */
---
+++ src/lib/stdio.c
@@ -0,0 +1,22 @@
+/* stdio.c
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <syscall.h>
+
+extern int vsprintf(char *buf, const char *fmt, va_list args);
+
+uint32_t printf(const char *fmt, ...)
+{
+	va_list args;
+	int i;
+    char buf[1024] = { 0 };
+
+	va_start(args, fmt);
+	i = vsprintf(buf,fmt,args);
+	va_end(args);
+	write(buf);
+
+	return i;
+}
