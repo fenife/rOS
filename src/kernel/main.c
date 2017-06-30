6c1057726ad5f792e4384238a8e97279afd7b040
feng <896151545@qq.com>
2017-06-30 16:07:49 +0800

    实现sys_read

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -36,10 +36,27 @@ int main(void)
     thread_start("k_thread_b", 31, k_thread_b, "k_thread_b");
 
     uint32_t fd = sys_open("/file1", O_RDWR);
-    printf("fd: %d\n", fd);
-    sys_write(fd, "hello world\n", 12);
+    printf("open /file1, fd:%d\n", fd);
+    char buf[64] = {0};
+    int read_bytes = sys_read(fd, buf, 18);
+    printf("1. read %d bytes:\n%s\n", read_bytes, buf);
+
+    memset(buf, 0, 64);
+    read_bytes = sys_read(fd, buf, 6);
+    printf("2. read %d bytes:\n%s", read_bytes, buf);
+
+    memset(buf, 0, 64);
+    read_bytes = sys_read(fd, buf, 6);
+    printf("3. read %d bytes:\n%s", read_bytes, buf);
+
+    printf("________  close file1 and reopen  ________\n");
     sys_close(fd);
-    printf("%d closed now\n", fd);
+    fd = sys_open("/file1", O_RDWR);
+    memset(buf, 0, 64);
+    read_bytes = sys_read(fd, buf, 24);
+    printf("4. read %d bytes:\n%s", read_bytes, buf);
+
+    sys_close(fd);
     
     while (1)
         ;
