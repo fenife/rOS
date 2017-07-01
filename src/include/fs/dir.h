72a3a37ae76af09acf1185981803ad640bca4cea
feng <896151545@qq.com>
2017-07-01 10:30:38 +0800

    实现目录删除和sys_rmdir

--- src/include/fs/dir.h
+++ src/include/fs/dir.h
@@ -45,6 +45,8 @@ int32_t dir_remove(struct dir *parent_dir, struct dir *child_dir);
 bool delete_dir_entry(struct partition *part, struct dir *pdir, 
                         uint32_t inode_no, void *io_buf);
 struct dir_entry* dir_read(struct dir* dir);
+bool dir_is_empty(struct dir* dir);
+int32_t dir_remove(struct dir* parent_dir, struct dir* child_dir);
 
 
 #endif  /* __FS_DIR_H */
